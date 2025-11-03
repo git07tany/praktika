5d57a55fd027323045ce4c6ab0c1c4d3489c4c11
git07tany <anashkina_2007@mail.ru>
2025-11-03 14:56:31 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import Girl from './Girl';
-import Header from './Header';
+import Girl from './components/Girl';
+import Header from './components/Header';
+import Documentation from './components/Documentation';
+import Customer from './components/Customer';
+import Room from './components/Room';
+import ServicesSection from './components/Services';
+import CustomersSection from './components/CustomersSection';
+import Orange from './components/Orange';
+import Testion from './components/Test';
+import Faq from './components/Faq'
 
 const App = () => {
   return (
     <div>
     <Header />
     <Girl/>
+    <ServicesSection/>
+    <Documentation/>
+    <Customer/>
+    <Room/>
+    <CustomersSection/>
+    <Faq/>
+    <Testion/>
+    <Orange/>
     </div>
   );
 };
